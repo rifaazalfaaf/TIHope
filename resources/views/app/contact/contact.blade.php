d3471aa3dbc4446ad1be02438d0a67e7e69828c3
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-11-11 19:55:06 +0700

    Membuat layout template untuk tampilan about dan contact us

---
+++ resources/views/app/contact/contact.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+@section('title','Contact Us')
+
+@section('styles')
+
+@endsection
+
+@section('content')
+{{-- masukin html nya disini --}}
+
+@endsection
+
+
+@section('scripts')
+
+@endsection
