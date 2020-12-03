3eddffbfcc1124c8f8b2e83853526be913e8d76a
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-12-03 10:16:51 +0700

    menghapus css yang ada dalm file contact, dntmbh margin dr bootstrap

--- resources/views/app/contact/contact.blade.php
+++ resources/views/app/contact/contact.blade.php
@@ -2,81 +2,7 @@
 @section('title','Contact')
 
 @section('styles')
-<style>
 
-.row .col-md-6 .sub-judul{
-width: 637px;
-height: 110px;
-left: 93px;
-top: 310px;
-
-font-family: Roboto;
-font-style: normal;
-font-weight: normal;
-font-size: 19px;
-line-height: 22px;
-letter-spacing: 0.2em;
-
-color: #362F29;
-}
-.card .ContacInfo .nama {
-
-width: 510px;
-height: 32.71px;
-left: 140px;
-top: 527.18px;
-
-font-family: Roboto;
-font-style: normal;
-font-weight: normal;
-font-size: 30px;
-line-height: 35px;
-display: flex;
-align-items: center;
-letter-spacing: 0.2em;
-	color : #EECB2D;
-
-}
-
-.card .ContacInfo .npm{
-
-width: 872.97px;
-height: 50.46px;
-left: 140px;
-top: 577.64px;
-
-
-font-family: Roboto;
-font-style: normal;
-font-weight: normal;
-font-size: 26px;
-line-height: 30px;
-display: flex;
-align-items: center;
-letter-spacing: 0.2em;
-
-color: #000000;
-}
-
-.card .ContacInfo .row .p{
-width: 872.97px;
-height: 50.46px;
-left: 246px;
-top: 690.71px;
-
-font-family: Roboto;
-font-style: normal;
-font-weight: normal;
-font-size: 26px;
-line-height: 30px;
-display: flex;
-align-items: center;
-letter-spacing: 0.2em;
-
-color: #000000;
-}
-
-</style>
 @endsection
 
 @section('content')
@@ -85,7 +11,7 @@ color: #000000;
 		<div class="row">
 			<div class="col-md-6">
 				<h1 class="judul mb-2 font1">Contact Us</h1>
-				<div class="col-md-7" >
+				<div class="col-md-7 p-0" >
 					<hr style="border: 1px solid #EECB2D">
 				</div>
 				<p class="sub-judul">apabila teman-teman memiliki materi terbaru silahkan hubungi kontak dibawah ini sesuai dengan angkatan masing-masing.</p>
@@ -95,7 +21,7 @@ color: #000000;
 		 	</div>
 		</div>
 		<div class="card">
-			<div class="ml-3">
+			<div class="ml-4 mt-4">
 			<div class="ContacInfo">
 				<a class="nama">Koordinator Angkatan 2017 </a>
 		        <p class="npm">Muhammad Fakhri Rahman</p>
