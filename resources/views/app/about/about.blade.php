e6389edbc83a7f44fbd3c215db2b1ae5dc060d96
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-12-03 10:13:44 +0700

    memindahkan css page about ke file css, dan memberi margin dr bootstrap

--- resources/views/app/about/about.blade.php
+++ resources/views/app/about/about.blade.php
@@ -2,89 +2,23 @@
 @section('title','About')
 
 @section('styles')
-<style>
 
-.card{
-	background-color :  #F5F5F5;
-
-}
-
-.card .ContacInfo .nama {
-
-width: 510px;
-height: 32.71px;
-left: 140px;
-top: 527.18px;
-
-font-family: Roboto;
-font-style: normal;
-font-weight: normal;
-font-size: 30px;
-line-height: 35px;
-display: flex;
-align-items: center;
-letter-spacing: 0.2em;
-	color : #EECB2D;
-
-}
-
-.card .ContacInfo .npm{
-
-width: 872.97px;
-height: 50.46px;
-left: 140px;
-top: 577.64px;
-
-
-font-family: Roboto;
-font-style: normal;
-font-weight: normal;
-font-size: 26px;
-line-height: 30px;
-display: flex;
-align-items: center;
-letter-spacing: 0.2em;
-
-color: #000000;
-}
-
-.card .ContacInfo .row .p{
-width: 872.97px;
-height: 50.46px;
-left: 246px;
-top: 690.71px;
-
-font-family: Roboto;
-font-style: normal;
-font-weight: normal;
-font-size: 26px;
-line-height: 30px;
-display: flex;
-align-items: center;
-letter-spacing: 0.2em;
-
-color: #000000;
-}
-
-</style>
 @endsection
 
 @section('content')
 {{-- masukin html nya disini --}}
 	<div class = "container">
-		<div class="row">
-			<div class="col-md-6">
+		<div class="row mb-3">
+			<div class="col-md-3">
 				<h1 class="judul mb-2 font1">About Us</h1>
-				<div class="col-md-6" >
-					<hr style="border: 1px solid #EECB2D">
-				</div>
+				<hr style="border: 1px solid #EECB2D">
 			</div>
-			<div class="col-md-6">
+			<div class="col-md-9" align="right">
 		 		<img src="{{asset('img/About us page-amico.png')}}" alt="Image" height="406" width="423">
 		 	</div>
 		</div>
 		<div class="card">
-			<div class="ml-3">
+			<div class="ml-4 mt-4">
 			<div class="ContacInfo">
 				<a class="nama">Junia Adhani Juzar </a>
 		        <p class="npm">140810170015</p>
